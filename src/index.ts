f1aecf0b777bafd8164c492ddb03832e5a2dcd74
svetlana <developer-svetlana@yandex.ru>
2022-07-29 17:25:44 +0500

    dz5

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import { renderSearchStubBlock } from "./search-results.js";
 import { renderUserBlock } from "./user.js";
 import { renderToast } from "./lib.js";
 
+
 window.addEventListener("DOMContentLoaded", () => {
   renderUserBlock("Wade Warren", "0", 3);
-  renderSearchFormBlock(1, 8);
+  renderSearchFormBlock();
   renderSearchStubBlock();
   renderToast(
     {
--- src/search-form.ts
+++ src/search-form.ts
@@ -1,24 +1,25 @@
 import { renderBlock } from "./lib.js";
+//import { today, defaultStart, defaultFinish, maxDayFinish } from './dates.js'
+//import { searchHandler } from './helpers/search-handler.js'
+import { baseUrl1 } from './API/index.js';
+import { baseUrl2 } from './SDK/index.js';
+import { renderSearchResultBlock} from './search-results.js'
 
-export function renderSearchFormBlock(dayStart, dayFinish) {
- 
- let today = new Date();
- console.log(today);
- 
- let maxDayFinish = new Date(today.getFullYear(), today.getMonth()+2, 0);
- console.log(maxDayFinish);
- 
- let defaultStart = new Date(today.getFullYear(), today.getMonth(), today.getDate()+1);
- console.log(defaultStart);
- 
- let defaultFinish = new Date(today.getFullYear(), today.getMonth(), today.getDate()+3);
- console.log(defaultFinish);
+export let today = new Date();
+console.log(today);
 
- //document.getElementById('check-in-date').innerHTML = today;
- //document.getElementById('check-out-date').innerHTML = maxDayFinish;
+export let maxDayFinish = new Date(today.getFullYear(), today.getMonth() + 2, 0);
+console.log(maxDayFinish);
 
+export let defaultStart = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+console.log(defaultStart);
+
+export let defaultFinish = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 3);
+console.log(defaultFinish);
+
+export function renderSearchFormBlock(checkInDate?: Date, checkOutDate?: Date) {
  
- renderBlock(
+  renderBlock(
   'search-form-block',
   `
   <form>
@@ -37,11 +38,11 @@ export function renderSearchFormBlock(dayStart, dayFinish) {
       <div class="row">
         <div>
           <label for="check-in-date">Дата заезда</label>
-          <input id="check-in-date" type="date" value="2022-07-01" min="2022-07-01" max="2022-08-31" name="checkin" />
+          <input id="check-in-date" type="date" value="${checkInDate || defaultStart}" min="${today}" max="${maxDayFinish}" name="checkin" />
         </div>
         <div>
           <label for="check-out-date">Дата выезда</label>
-          <input id="check-out-date" type="date" value="2022-08-31" min="2022-07-03" max="2022-08-31" name="checkout" />
+          <input id="check-out-date" type="date" value="${checkOutDate || defaultFinish}" min="${today}" max="${maxDayFinish}" name="checkin" />
         </div>
         <div>
           <label for="max-price">Макс. цена суток</label>
@@ -55,6 +56,24 @@ export function renderSearchFormBlock(dayStart, dayFinish) {
   </form>
   `
 )
+const btnSearch = document.getElementById('btn-search');
+btnSearch.addEventListener('click', (event: MouseEvent) => {
+  event.preventDefault();
+fetchPlaces();
+})
+function fetchPlaces() {
+const coordinates = '59.9386.30.3141';
+const checkInDate = new Date(today).getTime() || new Date(defaultStart).getTime();
+const checkOutDate = new Date(maxDayFinish).getTime() || new Date(defaultFinish).getTime();
+
+  fetch(baseUrl2 + `/places?coordinates=${coordinates}&chekInDate=${checkInDate}&checkOutDate=${checkOutDate}&maxPrice=10000`)
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    console.log(data)
+    renderSearchResultBlock(data)
+  })
 }
 
-
+}
